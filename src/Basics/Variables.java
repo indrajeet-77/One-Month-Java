64c3415d1bc952f350f37898382088272590689c
indrajeet-77 <indrajeetpimpalgaonkar376@gmail.com>
2026-05-12 20:05:46 +0530

    first commit

---
+++ src/Basics/Variables.java
@@ -0,0 +1,18 @@
+package Basics;
+public class Variables {
+    public static void main(String[] args) {
+        
+        String Name="Indrajeet";
+        char Grade ='A';
+        int Rank=10;
+        double CGPA=7.91;
+        
+        System.out.println("My name is "+Name);
+        System.out.println("I Got "+Grade+" Grades");
+        System.out.println("My rank is"+ Rank);
+        System.out.println("My CGPA is"+CGPA);
+
+
+    }
+    
+};
