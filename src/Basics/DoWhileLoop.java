18b8b7476b557ab31d27dcf60a204710300bfdcf
indrajeet-77 <indrajeetpimpalgaonkar376@gmail.com>
2026-05-13 22:32:18 +0530

    Done with the loops concept in java

---
+++ src/Basics/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class DoWhileLoop {
+    public static void main(String[] args){
+        int i=0;
+        do{
+            System.out.println("executed "+i);
+            i++;
+
+        }
+        while (i<5);
+
+    }
+    
+}
